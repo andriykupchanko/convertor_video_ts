61feec0e9b278016965f5b7d632629ae7f4dba23
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2023-12-15 11:38:33 +0200

    create input processing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
-class App{
-    run(){
-        console.log("run");
-    }
+import { PromptService } from './core/prompt/prompt.service';
+
+export class App {
+	async run() {
+		const res = await (new PromptService()).input<number>('Число', 'number');
+		console.log(res);
+	}
 }
+
 const app = new App();
 app.run();
